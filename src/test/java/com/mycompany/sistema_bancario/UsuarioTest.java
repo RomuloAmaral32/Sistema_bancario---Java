7438639e3a57a263e9e33fffb33b9a20ad2d3c04
DarlanHenrique <darlan.silva@ice.ufjf.br>
2025-02-14 22:15:12 -0300

    criando teste de usuario

---
+++ src/test/java/com/mycompany/sistema_bancario/UsuarioTest.java
@@ -0,0 +1,190 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
+ */
+package com.mycompany.sistema_bancario;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author Darlan
+ */
+public class UsuarioTest {
+    
+    public UsuarioTest() {
+    }
+    
+    @BeforeAll
+    public static void setUpClass() {
+    }
+    
+    @AfterAll
+    public static void tearDownClass() {
+    }
+    
+    @BeforeEach
+    public void setUp() {
+    }
+    
+    @AfterEach
+    public void tearDown() {
+    }
+
+    /**
+     * Test of getNome method, of class Usuario.
+     */
+    @Test
+    public void testGetNome() {
+        System.out.println("getNome");
+        Usuario instance = null;
+        String expResult = "";
+        String result = instance.getNome();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of setNome method, of class Usuario.
+     */
+    @Test
+    public void testSetNome() {
+        System.out.println("setNome");
+        String nome = "";
+        Usuario instance = null;
+        instance.setNome(nome);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getCpf method, of class Usuario.
+     */
+    @Test
+    public void testGetCpf() {
+        System.out.println("getCpf");
+        Usuario instance = null;
+        String expResult = "";
+        String result = instance.getCpf();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of setCpf method, of class Usuario.
+     */
+    @Test
+    public void testSetCpf() {
+        System.out.println("setCpf");
+        String cpf = "";
+        Usuario instance = null;
+        instance.setCpf(cpf);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getSenha method, of class Usuario.
+     */
+    @Test
+    public void testGetSenha() {
+        System.out.println("getSenha");
+        Usuario instance = null;
+        String expResult = "";
+        String result = instance.getSenha();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of setSenha method, of class Usuario.
+     */
+    @Test
+    public void testSetSenha() {
+        System.out.println("setSenha");
+        String senha = "";
+        Usuario instance = null;
+        instance.setSenha(senha);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getEmail method, of class Usuario.
+     */
+    @Test
+    public void testGetEmail() {
+        System.out.println("getEmail");
+        Usuario instance = null;
+        String expResult = "";
+        String result = instance.getEmail();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of setEmail method, of class Usuario.
+     */
+    @Test
+    public void testSetEmail() {
+        System.out.println("setEmail");
+        String email = "";
+        Usuario instance = null;
+        instance.setEmail(email);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getTipo method, of class Usuario.
+     */
+    @Test
+    public void testGetTipo() {
+        System.out.println("getTipo");
+        Usuario instance = null;
+        String expResult = "";
+        String result = instance.getTipo();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of setTipo method, of class Usuario.
+     */
+    @Test
+    public void testSetTipo() {
+        System.out.println("setTipo");
+        String tipo = "";
+        Usuario instance = null;
+        instance.setTipo(tipo);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of login method, of class Usuario.
+     */
+    @Test
+    public void testLogin() {
+        System.out.println("login");
+        String cpfInserido = "";
+        String senhaInserida = "";
+        Usuario instance = null;
+        boolean expResult = false;
+        boolean result = instance.login(cpfInserido, senhaInserida);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+}
