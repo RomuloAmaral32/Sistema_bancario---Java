3a9f729c812132e601545fbc6843b518a251364d
RomuloAmaral <romuloamaral32@gmail.com>
2025-02-18 13:53:40 -0300

    adiciona verificação de senha para certas operações e testa os métodos

--- src/main/java/com/mycompany/sistema_bancario/Cliente.java
+++ src/main/java/com/mycompany/sistema_bancario/Cliente.java
@@ -2,76 +2,98 @@ package com.mycompany.sistema_bancario;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
-public class Cliente extends Usuario{
+public class Cliente extends Usuario {
     private String contaBancaria;
     private String endereco;
     private double saldo;
     private List<String> extrato;
+    private Scanner scanner;  
 
-    public Cliente(String nome, String cpf, String senha, String email, String tipo, String contaBancaria, String endereco, double saldoInicial){
+    public Cliente(String nome, String cpf, String senha, String email, String tipo, String contaBancaria, String endereco, double saldoInicial) {
         super(nome, cpf, senha, email, tipo);
         this.contaBancaria = contaBancaria;
         this.endereco = endereco;
         this.saldo = saldoInicial;
         this.extrato = new ArrayList<>();
+        this.scanner = new Scanner(System.in);  
         registrarExtrato("Abertura da conta com saldo inicial: R$" + saldoInicial);
-    } 
-
-    public double consultarSaldo(){
-        return this.saldo;
     }
 
-    public transferir(double valor, cliente destinatario){
-        if(valor <= this.saldo){
-            this.saldo -= valor;
-            destinatario.receberTransferencia(valor, this);
-            registrarExtrato("Transferencia de R$" + valor + "para" + destinatario.getNome());
-        }
-        else{
-            System.out.println("Saldo insuficiênte para esse valor de transferência");
+    // Método de validação de senha
+    private boolean validarSenha() {
+        System.out.print("Digite sua senha para validar a operação: ");
+        String senhaDigitada = scanner.nextLine();  
+        return senhaDigitada.equals(super.getSenha());
+    }
+
+    public void consultarSaldo() {
+        if (validarSenha()) {
+            System.out.println("Seu saldo é: R$"+ this.saldo);
+        } else {
+            System.out.println("Senha incorreta."); 
         }
     }
 
-    public receberTransferencia(double valor, cliente remetente){
+    public void transferir(double valor, Cliente destinatario) {
+        if (validarSenha()) {
+            if (valor <= this.saldo) {
+                this.saldo -= valor;
+                destinatario.receberTransferencia(valor, this);
+                registrarExtrato("Transferência de R$" + valor + " para " + destinatario.getContaBancaria());
+                System.out.println("Transferência realizada com sucesso.");
+            } else {
+                System.out.println("Saldo insuficiente para essa transferência.");
+            }
+        } else {
+            System.out.println("Senha incorreta. Operação cancelada.");
+        }
+    }
+
+    public void receberTransferencia(double valor, Cliente remetente) {
         this.saldo += valor;
-        registrarExtrato("Recebido R$" + valor + "de" + remetente.getNome());
+        registrarExtrato("Recebido R$" + valor + " de " + remetente.getContaBancaria());
     }
 
-    public void sacar(double valor){
-        if(valor <= this.saldo){
-            this.saldo -=valor;
-            registrarExtrato("Saque de R$" + valor);
-            System.out.println("Saque de R$" + valor + "realizado com sucesso.");
-        }
-        else{
-            System.out.println("Saldo insuficiente para esse valor para saque");
-        }
-    }
-
-
-    public void consultarExtrato(){
-        System.out.println("Extrato da conta " + this.contaBancaria + ":");
-        for(String registro : extrato){
-            System.out.println(registro);
+    public void sacar(double valor) {
+        if (validarSenha()) {
+            if (valor <= this.saldo) {
+                this.saldo -= valor;
+                registrarExtrato("Saque de R$" + valor);
+                System.out.println("Saque de R$" + valor + " realizado com sucesso.");
+            } else {
+                System.out.println("Saldo insuficiente para esse saque.");
+            }
+        } else {
+            System.out.println("Senha incorreta. Operação cancelada.");
         }
     }
 
-    public void registrarExtrato(String transacao){
+    public void consultarExtrato() {
+        if (validarSenha()) {
+            System.out.println("Extrato da conta " + this.contaBancaria + ":");
+            for (String registro : extrato) {
+                System.out.println(registro);
+            }
+        } else {
+            System.out.println("Senha incorreta. Operação cancelada.");
+        }
+    }
+
+    private void registrarExtrato(String transacao) {
         extrato.add(transacao);
     }
 
-    public String getContaBancaria(){
+    public String getContaBancaria() {
         return contaBancaria;
     }
 
-    public String getEndereco(){
+    public String getEndereco() {
         return endereco;
     }
 
-    public String setEndereco(String endereco){
+    public void setEndereco(String endereco) {
         this.endereco = endereco;
     }
-
-
 }
--- src/main/java/com/mycompany/sistema_bancario/Main.java
+++ src/main/java/com/mycompany/sistema_bancario/Main.java
@@ -1,27 +1,31 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package com.mycompany.sistema_bancario;
 
-
-/**
- *
- * @author Darlan Henrique da Costa Silva
- * @matricula 202176038
- */
 public class Main {
 
-    public static void main(String[] args) {
-        Usuario usuario = new Usuario("Darlan", "12345678900", "123456", "darlan@emaill.com", "cliente");
-        String cpfInserido = "12345678900";
-        String senhaInserida = "123456"; 
-        boolean loginValido = usuario.login(cpfInserido, senhaInserida);
+    public static void main(String[] args) {                        //Main feita somente para visualizar operaçoes e validacoes--(nao é o resultado final)
 
-        if (loginValido) 
-            System.out.println("Login bem-sucedido! Bem-vindo, " + usuario.getNome() + ".");
-        else
-            System.out.println("Login falhou. Verifique seu CPF e senha.");
+        Cliente cliente1 = new Cliente("Kauã", "161.586.406-70", "senha123", "kaua@email.com", "Cliente", "12345-6", "Rua A, 123", 1000.0);
+        Cliente cliente2 = new Cliente("João", "161.586.406-70", "senha456", "joao@email.com", "Cliente", "54321-0", "Rua B, 321", 500.0);
+        
+        System.out.println("---- Cliente 1: Kauã ----");
+        cliente1.consultarSaldo(); 
+        
+        System.out.println("\n---- Transferindo de Kauã para João ----");
+        cliente1.transferir(200.0, cliente2); 
+       
+        System.out.println("\n---- Consultando extrato de Kauã ----");
+        cliente1.consultarExtrato(); 
+        
+        System.out.println("\n---- Consultando extrato de João ----");
+        cliente2.consultarExtrato(); 
 
+        System.out.println("\n---- Sacando da conta de Kauã ----");
+        cliente1.sacar(300.0); 
+
+        System.out.println("\n---- Consultando saldo de Kauã ----");
+        cliente1.consultarSaldo();
+
+        System.out.println("\n---- Consultando extrato de Kauã ----");
+        cliente1.consultarExtrato();
     }
 }
